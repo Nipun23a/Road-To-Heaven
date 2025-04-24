5ede55138853139cd149fb5d64bd074ea7f172dd
Nipuna Kaweya <nipuna609@gmail.com>
2025-04-24 18:10:31 +0530

    Home Updated

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import HeroSection from "@/components/HeroSection";
+import DestinationsShowcase from "@/components/DestinationShowcase";
 
 
 export default function Home() {
   return (
       <main>
           <HeroSection/>
+
+          <DestinationsShowcase/>
 
           {/* Additional content sections can go here */}
           <div className="grid items-center justify-items-center p-8 sm:p-20 gap-16">
